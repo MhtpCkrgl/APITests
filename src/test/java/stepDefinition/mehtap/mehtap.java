8f2819b0bc32109e223ebc9ecdb4e538d8f8a92c
Mehtap Cakiroglu <mhtpckrgl@gmail.com>
2023-12-06 22:33:52 +0300

    0612MC2

---
+++ src/test/java/stepDefinition/mehtap/mehtap.java
@@ -0,0 +1,60 @@
+package stepDefinition.mehtap;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import testData.SimpleBookTestData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static base_url.BaseUrl.spec;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static utilities.ObjectMapperUtils.convertJsonToJava;
+
+public class mehtap {
+    Response response;
+    @Given("mc user sets url")
+    public void mcUserGoesUrl() {
+        //set the url
+        spec.pathParam("first","orders");
+    }
+
+    @And("mc user send POST request to order book")
+    public void mcUserSendPOSTRequestToOrderBook() {
+        // set the expected data
+        SimpleBookTestData obj= new SimpleBookTestData();
+        Map<String,Object> expectedData= obj.orderBookRequestMethod(7,"Mehtap C");
+
+
+
+        // send the request and get the response
+     response = given(spec).body(expectedData).when().post("{first}");
+        response.prettyPrint();
+    }
+
+    @Then("mc user verify status code is {int}")
+    public void mcUserVerifyStatusCodeIs(int arg0) {
+        assertTrue(response.statusCode()==201);
+    }
+
+    @Then("mc user validates the response body")
+    public void mcUserValidatesTheResponseBody() {
+        //do assertion
+        Map<String,String> actualData =convertJsonToJava(response.asString(), HashMap.class);
+        assertEquals(actualData.get("created"),true);
+      //orderId icin dogrulama yaopmak biraz sikinti cunku her seferinde farkli bir data dondurecek
+        //assertTrue(response.body("orderId",hasSize(1)));
+        assertEquals(201,response.statusCode());
+        assertEquals(response.contentType(),"application/json");
+        //response time dogrulamasi yapilabilir burada
+
+
+    }
+}
