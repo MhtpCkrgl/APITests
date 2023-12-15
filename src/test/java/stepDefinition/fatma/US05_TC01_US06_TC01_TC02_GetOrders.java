49b786f966d4e50ee6998c22c5e664c00da6be26
ftmtrm <fatostarim@gmail.com>
2023-12-15 16:01:21 +0300

    vson

--- src/test/java/stepDefinition/fatma/US05_TC01_US06_TC01_TC02_GetOrders.java
+++ src/test/java/stepDefinition/fatma/US05_TC01_US06_TC01_TC02_GetOrders.java
@@ -5,16 +5,45 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import testData.SimpleBookTestData;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 import static base_url.BaseUrl.spec;
 import static io.restassured.RestAssured.given;
+import static org.codehaus.groovy.runtime.StringGroovyMethods.contains;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertEquals;
 import static stepDefinition.fatma.US04_TC01_TC02_TC03_PostAnOrder.*;
 
 public class US05_TC01_US06_TC01_TC02_GetOrders {
         Response response;
+        static int limit=6;
+
+    @Given("User send POST request to order books")
+    public void userSendPOSTRequestToOrderBooks() {
+        for (int j = 0; j < limit; j++) {
+            spec.pathParam("first", "orders");
+            SimpleBookTestData sentBody = new SimpleBookTestData();
+            Random random = new Random();
+            Map<String, Object> expectedData;
+            for (int i = 0; i < 7; i++) {
+                bookId = random.nextInt(7);
+                if (bookId == 2 || bookId == 0) {
+                    continue;
+                }
+                expectedData = sentBody.orderBookRequestMethod(bookId, customerName);
+                System.out.println(bookId);
+                response = given(spec).body(expectedData).post("{first}");
+                response.prettyPrint();
+                break;
+            }
+        }
+    }
 
         @And("User send GET request to get the ordered books")
         public void user_send_get_request_to_get_the_ordered_books() {
@@ -25,12 +54,14 @@ public class US05_TC01_US06_TC01_TC02_GetOrders {
 
         @Then("Verify the response body consists of the list of ordered books")
         public void verify_the_response_body_consists_of_the_list_of_ordered_books() {
-//            response.then()
-//                    .statusCode(200)
-//                    .body("bookId", hasItems(4, 5),
-//                            "customerName", hasItems("Damon Collins", "Sergio Considine", "Mrs. Hope Stoltenberg",
-//                                    "Mrs. Byron Nolan", "Virginia Barrows", "Dora Walsh", "Fatma Rau", "Duane Howell"),
-//                            hasSize(8), Matchers.containsString("1701719337733"));
+            response.then().statusCode(200).body("id",hasSize(limit),
+                    "quantity",equalTo(List.of(1,1,1,1,1,1)));
+            JsonPath jsonPath = response.jsonPath();
+            int size1 = jsonPath.getList("timestamp").size();
+            assertEquals(limit,size1);
+            int size2 = jsonPath.getList("quantity").size();
+            assertEquals(limit,size2);
+
         }
 
     @Given("User send GET request to get an ordered books using valid orderId")
@@ -68,6 +99,8 @@ public class US05_TC01_US06_TC01_TC02_GetOrders {
         assertEquals("No order with id abfytdrskouy.",errorMessage);
 
     }
+
+
 }
 
 
